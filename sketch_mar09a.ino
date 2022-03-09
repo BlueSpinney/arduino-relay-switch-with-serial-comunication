4e1e59f950672554999cfeb589dd1115fe6eb3cc
BlueSpinney <99137204+BlueSpinney@users.noreply.github.com>
2022-03-09 20:16:07 +0100

    led_pulse

---
+++ sketch_mar09a.ino
@@ -0,0 +1,17 @@
+
+int ledpin = 9;
+
+
+
+void setup() {
+  pinMode(ledpin,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(ledpin,HIGH);
+  delay(1000);
+  digitalWrite(ledpin,LOW);
+  delay(1000);
+
+}
